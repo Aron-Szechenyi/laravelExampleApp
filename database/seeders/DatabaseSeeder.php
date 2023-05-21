bb9a4757366d9f8ca6f3554363bd583816a1b807
Aron Szechenyi <aronszechenyi@gmail.com>
2023-05-21 17:54:32 +0200

    migration and seeder updatet

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
 use App\Models\User;
+use Database\Factories\CompanyFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        // User
         User::create([
             'username'=>'admin',
             'email'=>'test1@test.com',
@@ -26,5 +29,6 @@ class DatabaseSeeder extends Seeder
             'email'=>'test2@test.com',
             'password'=>Hash::Make('root')
         ]);
+        Company::factory()->count(15)->create();
     }
 }
